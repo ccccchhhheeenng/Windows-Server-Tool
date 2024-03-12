bc7d0ceeb52fa7bb49c168b211633eb5030d552d
ccccchhhheeenng <ccccchhhheeenng@gmail.com>
2024-03-12 12:22:32 +0800

    Upload File

---
+++ main.py
@@ -0,0 +1,24 @@
+import tkinter as tk
+import time
+import threading
+root = tk.Tk()
+import os
+root.title('my window')
+root.geometry('200x150')
+
+def button_event():
+    os.system("start cmd")  
+    mybutton.config(text='complete') 
+    after_thread.start()
+
+def run_after_completion():
+    time.sleep(3)  
+    mybutton.config(text="run cmd")  
+
+mybutton = tk.Button(root, text='run cmd', command=button_event)
+mybutton.pack()
+
+
+after_thread = threading.Thread(target=run_after_completion)
+
+root.mainloop()
