050a22e84d56871ddae40e1310e46e50569a4864
ccccchhhheeenng <98439044+ccccchhhheeenng@users.noreply.github.com>
2024-09-13 12:31:06 +0800

    Update at 2024/09/13 12:31

--- test.py
+++ test.py
@@ -7,13 +7,18 @@ from tkinter import ttk
 root = tk.Tk()
 root.title('Windows Server Setup')
 root.geometry('400x400')
+current_interface = list()
 #Style
 style = ttk.Style()
 style.configure('Custom.TButton', font=('Helvetica', 12), foreground='blue', padding=5)
 style.configure('Red.TButton', font=('Helvetica', 12), foreground='red', padding=5)
 style.configure('Green.TButton', font=('Helvetica', 12), foreground='green', padding=5)
 
-current_interface = list()
+#powershell
+def powershell(command):
+    subprocess.run(["powershell.exe", command])
+
+#-----<Interface>-----
 def update_interface():
     for widget in root.winfo_children():
         widget.destroy()
@@ -23,8 +28,9 @@ def update_interface():
         setup_DHCP_interface()
     if "DNS" in current_interface:
         setup_DNS_interface()
-
-#######################
+    if "RemoveDHCP" in current_interface:
+        Remove_DHCP_Scope_interface()
+#---------------------
 def back():
     current_interface.pop()
     update_interface()
@@ -34,19 +40,96 @@ def DHCP_Setup_Click():
 def DNS_Setup_click():
     current_interface.append("DNS")
     update_interface()
-########################
+def Remove_DHCP_Scope_Click():
+    current_interface.append("RemoveDHCP")
+    update_interface()
+#-----</Interface>-----
 
+#-----</Main Interface>-----
 def setup_main_interface():
-  # DHCP_Install = ttk.Button(root, text='Install DHCP Feature', command=DHCP_Install_Click, style='Custom.TButton')
-  # DHCP_Install.pack()
-  # DHCP_Uninstall = ttk.Button(root, text='Uninstall DHCP Feature', command=DHCP_Uninstall_Click, style='Custom.TButton')
-  # DHCP_Uninstall.pack()
+
+#-------<Install tools>-----
+
+    #-----<DHCP_Install>-----
+    def DHCP_Install_Click():
+        DHCP_install_thread = threading.Thread(target=installing_DHCP)
+        DHCP_install_thread.start()
+        DHCP_Install.config(text='Installing')
+
+    def installing_DHCP():
+        powershell("Install-WindowsFeature -Name 'DHCP' –IncludeManagementTools")
+        DHCP_complete_thread = threading.Thread(target=Func_DHCP_complete)
+        DHCP_complete_thread.start()
+        DHCP_Install.config(text="Finished")  
+
+    def Func_DHCP_complete():
+        time.sleep(3)
+        DHCP_Install.config(text="Install DHCP Feature")  
+    #-----</DHCP_Install>-----
+        
+    #-----<DHCP_Uninstall>-----
+    def DHCP_Uninstall_Click():
+        DHCP_Uninstall_thread = threading.Thread(target=Uninstalling_DHCP)
+        DHCP_Uninstall_thread.start()
+        DHCP_Uninstall.config(text='Uninstalling')
+
+    def Uninstalling_DHCP():
+        powershell("Uninstall-WindowsFeature -Name 'DHCP' –IncludeManagementTools")
+        DHCP_complete_thread = threading.Thread(target=Func_UnDHCP_complete)
+        DHCP_complete_thread.start()
+
+    def Func_UnDHCP_complete():
+        DHCP_Uninstall.config(text="Finished")  
+        time.sleep(3)
+        DHCP_Uninstall.config(text="Uninstall DHCP Feature")  
+    #-----</DHCP_Uninstall>-----
+    
+#-------</Install tools>-----
+
+    #-----<Main Window Buttons>-----
+    DHCP_Install = ttk.Button(root, text='Install DHCP Feature', command=DHCP_Install_Click, style='Custom.TButton')
+    DHCP_Install.pack()
+    DHCP_Uninstall = ttk.Button(root, text='Uninstall DHCP Feature', command=DHCP_Uninstall_Click, style='Custom.TButton')
+    DHCP_Uninstall.pack()
     Setup_DHCP = ttk.Button(root, text="Setup DHCP", command=DHCP_Setup_Click, style='Custom.TButton')
     Setup_DHCP.pack()
     Setup_DNS=ttk.Button(root,text="Setup DNS",command=DNS_Setup_click, style='Custom.TButton')
     Setup_DNS.pack()
+    Remove_DHCP_Scope=ttk.Button(root,text="Remove DHCP Scope",command=Remove_DHCP_Scope_Click, style='Custom.TButton')
+    Remove_DHCP_Scope.pack()    
+    #-----</Main Window Buttons>-----
+#-----</Main Interface>-----
 
+#-----<DHCP Interface>-----
 def setup_DHCP_interface():
+    def DHCP_Finish_Button():
+        DHCP_Setup_thread = threading.Thread(target=DHCP_Finish_Button_Run)
+        DHCP_Setup_thread.start()
+        read_input.config(text="Please Wait")
+        
+    #<Get user input data>
+    def DHCP_Finish_Button_Run():
+        StartRange=StartRangeentry.get()
+        EndRange=EndRangeentry.get()
+        SubnetMask=SubnetMaskentry.get()
+        ScopeName=ScopeNameentry.get()
+        DNS_Address=DNS_Addressentry.get()
+        Router=Routerentry.get()
+        #<Scope id calculate>
+        r=StartRange.split(".")
+        ScopeID=""
+        for i in range(3):
+            ScopeID+=r[i]
+            ScopeID+="."
+        ScopeID+="0"
+        #</Scope id calculate>
+        AddScope="Add-DhcpServerV4Scope -Name "+ScopeName+" -StartRange "+StartRange+" -EndRange "+EndRange+" -Subnetmask "+SubnetMask
+        powershell(AddScope)
+        SetDHCPDNS="Set-DhcpServerv4OptionValue -ScopeId "+ScopeID+" -OptionId 6 -Value "+DNS_Address
+        powershell(SetDHCPDNS)
+        SetDHCPRouter="Set-DhcpServerv4OptionValue -ScopeId "+ScopeID+" -Router "+Router
+        powershell(SetDHCPRouter)
+        back()      
     StartRangelabel = tk.Label(root, text='StartRange:')
     StartRangelabel.grid(row=0, column=0)
     StartRangeentry = tk.Entry(root)
@@ -71,9 +154,32 @@ def setup_DHCP_interface():
     Routerlabel.grid(row=5, column=0)
     Routerentry = tk.Entry(root)
     Routerentry.grid(row=5, column=1)
-    Exit = ttk.Button(root, text="Back", command=back, style='Red.TButton')
+    Exit = ttk.Button(root, text="Exit", command=back, style='Red.TButton')
     Exit.grid(row=6, column=0, padx=20)
+    read_input=ttk.Button(root, text="Finish", command=DHCP_Finish_Button, style='Green.TButton')
+    read_input.grid(row=6,column=1)
+#-----</DHCP Interface>-----
 
+
+def Remove_DHCP_Scope_interface():
+    def DHCP_Finish_Button():
+        DHCP_Setup_Thread=threading.Thread(target=DHCP_Finish_Button_Run)
+        read_input.config(text="Please Wait")
+        DHCP_Setup_Thread.start()
+    def DHCP_Finish_Button_Run():
+        ScopeID=ScopeIDentry.get()
+        command="Remove-DhcpServerv4Scope -ScopeId "+ScopeID
+        powershell(command)
+        back()
+
+    ScopeIDlabel = tk.Label(root, text='ScopeID:')
+    ScopeIDlabel.grid(row=0, column=0)
+    ScopeIDentry = tk.Entry(root)
+    ScopeIDentry.grid(row=0, column=1)
+    Exit=ttk.Button(root, text="Exit", command=back, style='Red.TButton')
+    Exit.grid(row=6,column=0,padx=20)
+    read_input=ttk.Button(root, text="Finish", command=DHCP_Finish_Button, style='Green.TButton')
+    read_input.grid(row=6,column=1) 
 def setup_DNS_interface():
     if "Foward_Lookup_Zone" in current_interface:
         def Add_Primary_Button_Click():
